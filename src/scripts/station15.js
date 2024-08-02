e130c5834cae3c1cb0d03d92f6a12081af9895d2
Sugar1201 <140928.miina@gmail.com>
2024-08-02 20:43:53 +0900

    Complete task

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,29 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function buildFullName(data) {
+  data.full_name = data.family_name + ' ' + data.first_name;
+  return data;
 }
 
+function test(userList) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(() => {
+      try {
+        const fullNames = userList.map(buildFullName);
+        resolve(fullNames);
+      } catch (error) {
+        reject("Error generating full names");
+      }
+    }, 3000); // 3000 milliseconds, which is equal to 3 seconds
+  });
+}
+
+myPromise.then(
+  function(result) {myDisplayer(result);},
+  function(error) {myDisplayer(error);}
+);
